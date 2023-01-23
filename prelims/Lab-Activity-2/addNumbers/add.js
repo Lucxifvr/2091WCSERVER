8bf8546a9c86d91eb1a707e09360b07f8e903937
Kiana Malong <89562325+Lucxifvr@users.noreply.github.com>
2023-01-23 21:00:47 +0800

    Add files via upload

---
+++ prelims/Lab-Activity-2/addNumbers/add.js
@@ -0,0 +1,8 @@
+// Malong, Kiana Joi
+// WD 201
+
+function add(num1, num2) {
+    let y = num1 + num2;
+    return y;
+  }
+  module.exports = add;
---
+++ prelims/Lab-Activity-2/addNumbers/app.js
@@ -0,0 +1,6 @@
+// Malong, Kiana Joi
+// WD 201
+
+const add = require('./add.js');
+
+console.log('The sum of the two numbers is ' + add(12, 10));
