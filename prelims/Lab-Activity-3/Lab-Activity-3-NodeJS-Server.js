4201f5f96bba3d7467a42f8b3b4ca846dd8a219b
Lucxifvr <89562325+Lucxifvr@users.noreply.github.com>
2023-01-30 12:59:45 +0800

    1 file modified

--- prelims/Lab-Activity-3/Lab-Activity-3-NodeJS-Server.js
+++ prelims/Lab-Activity-3/Lab-Activity-3-NodeJS-Server.js
@@ -4,34 +4,43 @@
 
 // Use require() to import local module
 
-var http = require('http'); 
+var http = require('http');
 
-var server = http.createServer(function(req,res){
-    if(req.url == '/(root)'){
-        res.writeHead(200, {'Content-Type: texr/html'});
-        res.write('<html><body><p>Welcome to my Node.js Application')
-        res.write('<html><body><p>Welcome John Smit. This is an activity about basics of Node.js</p></body></html>');
-        res.end();
-    }
-    else if (req.url == "/about"){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><p>This is the About Page</p></body></html>')
-        res.write('<html><body><p>Hello John Smith. This activity will teach on how to deal with a simple server and local module in Node.js</p></body></html>');
-        res.end();
-    }
-    else if (req.url == "/contact"){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><p>This is the Contact Page</p></body></html>')
-        res.write('<html><body><p>John Smith, if you want additional deatails about this activity go to thuse site: https://wwww.tutorialsteacher.com/nodejs/nodejs-tutorials</p></body></html>');
-        res.end();
-    }
-    else if (req.url == "/gallery"){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><p>This is the Gallery Page</p></body></html>');
-        res.end();
-    }
-    else
-    res.end('Invalid Request');
+var server = http.createServer(function (req, res) {
+if (req.url == '/') {
+res.writeHead(200, { 'Content-Type': 'text/html' });
+
+res.write(
+'<html><body><h1>Welcome to my Node.js Application</h1></body></html>'
+);
+res.write(
+'<html><body><p>Welcome Kiana Malong. This is an activity about basics of Node.js</p></body></html>'
+);
+res.end();
+} else if (req.url == '/about') {
+res.writeHead(200, { 'Content-Type': 'text/html' });
+
+res.write('<html><body><h1>This is the About Page</h1></body></html>');
+res.write(
+'<html><body><p>Hello Kiana Malong. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+);
+res.end();
+} else if (req.url == '/contact') {
+res.writeHead(200, { 'Content-Type': 'text/html' });
+
+res.write('<html><body><h1>This is the Contact Page</h1></body></html>');
+res.write(
+'<html><body><p>Kiana Malong, if you want additional details about this activity go to this site: https://www.tutorialteacher.com/nodejs/nodejs-tutorials</p></body></html>'
+);
+res.end();
+} else if (req.url == '/contact') {
+res.writeHead(200, { 'Content-Type': 'text/html' });
+
+res.write('<html><body><h1>This is the Gallery Page</h1></body></html>');
+res.end();
+} else {
+res.end('Invalid Request');
+}
 });
+
 server.listen(5000);
-console.log('Node.js web server at port 5000 is running...')
