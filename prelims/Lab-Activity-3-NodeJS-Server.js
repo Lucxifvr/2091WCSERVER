2fb252d78848f52a561797f7bdfb0c30e4aa8d04
Lucxifvr <89562325+Lucxifvr@users.noreply.github.com>
2023-01-30 12:20:31 +0800

    1 file modified

--- prelims/Lab-Activity-3-NodeJS-Server.js
+++ prelims/Lab-Activity-3-NodeJS-Server.js
@@ -3,3 +3,12 @@
 //Section : WD 201
 
 // Use require() to import local module
+
+var http = require('http'); 
+
+var server = http.createServer(function(req,res){
+    if(req.url == '/'){
+        res.writeHead(200, {'Content-Type: texr/html'});
+        res.write('<html><body><p>Welcome to my Node.js Application')
+    }
+})
