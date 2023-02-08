b8e5f2669b7c7268423e7d56a35cc95afebd1ca6
Lucxifvr <89562325+Lucxifvr@users.noreply.github.com>
2023-02-08 15:54:01 +0800

    2 files added

---
+++ prelims/Prelim_Exam/app.js
@@ -0,0 +1,9 @@
+// Malong, Kiana Joi
+// WD 201
+
+var word = require('./message.js');
+var name = 'Kiana';
+
+console.log(word.greet(name));
+console.log(word.congratulations(name));
+console.log(word.farewell(name));
---
+++ prelims/Prelim_Exam/message.js
@@ -0,0 +1,15 @@
+// Malong, Kiana Joi
+// WD 201
+
+var word = {
+  greet: function (name) {
+    return 'Hello, ' + name + '!';
+  },
+  congratulations: function (name) {
+    return 'Congratulations, ' + name + '!';
+  },
+  farewell: function (name) {
+    return 'Goodbye, ' + name + '!';
+  },
+};
+module.exports = word;
