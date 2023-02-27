6b2b2c30a5e0515213672c80c245b68fdeff9213
Lucxifvr <89562325+Lucxifvr@users.noreply.github.com>
2023-02-27 11:57:38 +0800

    4 files modified

---
+++ Midterms/express.app/post-method/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+//require body-parser for this method
+var bodyParser = require('body-parser');
+const { response } = require('express');
+
+//Create application/x-www-form-urlencoded parser
+var urlecondedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.post('/process_post', urlecondedParser, function (req, res) {
+  //Prepare output in JSON format
+  response = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
+
+const port = process.env.PORT || 3500;
+
+app.listen(port, () => {
+  console.log(`Listening on  port ${port}`);
+});
