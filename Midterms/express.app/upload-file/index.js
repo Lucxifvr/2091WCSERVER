62d2488560f676af4abf333e97c19a1f21b10895
Lucxifvr <89562325+Lucxifvr@users.noreply.github.com>
2023-03-05 22:23:38 +0800

    1 file modified

--- Midterms/express.app/upload-file/index.js
+++ Midterms/express.app/upload-file/index.js
@@ -1,21 +1,61 @@
-//1. Installing Express
-//2. Installing libraries/dependencies
-//3. Require statements
-
 const express = require('express');
 const app = express();
 
-//4.use the middleware required for serving stat;
+//Uploads
 app.use(express.static('public'));
 
-//5.create the route to serve a static index.html
+const path = require('path');
+const mime = require('mime-types');
+const multer = require('multer');
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: fileStorage });
+
+app.post('/uploads', upload.single('myFile'), (req, res) => {
+  console.log(req.file);
+
+  req.file.mimetype = mime.lookup(req.file.originalname);
+
+  res.sendFile(path.join(__dirname, 'file-uploaded.html'));
+});
+
+app.get('/file-upload', (req, res) => {
+  res.sendFile(__dirname + '/' + 'file-upload.html');
+});
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/' + 'index.html');
 });
 
-//Setting the listener to ENV PORT info
-const port = process.env.PORT || 3000;
+//get form
 
-app.listen(port, () => {
-  console.log(`Listening on  port ${port}`);
+app.use(express.urlencoded());
+
+app.get('/', (req, res, next) => {
+  res.send(`<form method="POST" action="/">
+  <input type="text" name="name" placeholder="name">
+  <input type="text" name="subject" placeholder="subject">
+  <input type="text" name="email" placeholder="email" >
+  <input type="text" name="message" placeholder="message" >
+
+  <input type="submit">
+</form>`);
+});
+
+app.post('/', function (req, res, next) {
+  res.send(JSON.stringify(req.body));
+});
+
+//app listen server
+app.listen(3000, function () {
+  console.log('Server is running on port 3000');
 });
